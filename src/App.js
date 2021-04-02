f6ec346bc4e382999026fd9602d9d201717fe2dc
Joshua Julius <joshjulius1@gmail.com>
2021-04-01 21:32:29 -0500

    replace BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route } from "react-router-dom";
+import { HashRouter, Route } from "react-router-dom";
 import styles from "./App.module.css";
 import { Header, Main, Menu, About, Reservation, Footer } from "./components";
 
@@ -10,14 +10,14 @@ const App = () => {
   }
 
   return (
-    <BrowserRouter basename="/landing-page-project">
+    <HashRouter basename="/landing-page-project">
       <Header />
       <Route exact path="/" component={Main} />
       <Route path="/menu" component={Menu} />
       <Route path="/about" component={About} />
       <Route path="/reservation" component={Reservation} />
       <Footer />
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
