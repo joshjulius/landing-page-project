4e0d344036a9d87be0600747837a8c5a6328ce80
Joshua Julius <joshjulius1@gmail.com>
2021-04-03 22:34:05 -0500

    Updates

--- README.md
+++
@@ -1,4 +0,0 @@
-# landing-page-project
-
-A mock restaurant website made with React.
-Featuring a minimalist mobile first design, CSS animations, React Router.
--- src/components/index.js
+++
@@ -1,6 +0,0 @@
-export { default as Header } from "./Header";
-export { default as Main } from "./Main";
-export { default as Menu } from "./Menu";
-export { default as About } from "./About";
-export { default as Reservation } from "./Reservation";
-export { default as Footer } from "./Footer";
